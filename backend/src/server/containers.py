ebd2020db52ca35d8a32084db844ae0167e71eb4
Guilherme Lima Gonçalves <lwglguilherme@gmail.com>
2025-10-04 16:30:14 -0300

    refactor: add dependency injector container to app factory

---
+++ backend/src/server/containers.py
@@ -0,0 +1,43 @@
+from dependency_injector.containers import DeclarativeContainer
+from dependency_injector.providers import Configuration
+
+
+def build_db_config(config: Configuration) -> None:
+    config.db_engine.from_env("DB_ENGINE", as_=str, default="mongodb")    
+    config.db_name.from_env("DB_NAME", as_=str, default="cosmobras48_db")
+    config.db_host.from_env("DB_HOSTNAME", as_=str, default="0.0.0.0")
+    config.db_username.from_env("DB_USERNAME", as_=str, required=True)
+    config.db_password.from_env("DB_PASSWORD", as_=str, required=True)
+    config.db_port.from_env("DB_PORT", as_=int, default=21017)
+
+
+def build_server_config(config: Configuration) -> None:
+    config.server_hostname.from_env("SERVER_HOSTNAME", as_=str, default="0.0.0.0")
+    config.server_debug.from_env("SERVER_DEBUG", as_=bool, default=True)
+    config.server_port.from_env("SERVER_PORT", as_=int, default=8001)
+    config.server_log_level.from_env("SERVER_LOG_LEVEL", as_=str, default="info")
+    config.server_api_root_path.from_env("SERVER_API_ROOT_PATH", as_=str, default="api")
+    config.server_api_root_version.from_env("SERVER_API_ROOT_VERSION", as_=str, default="v1")
+    config.server_sysadmin_contact.from_env("SERVER_SYSADMIN_CONTACT", as_=str)
+
+
+def build_swagger_ui_config(config: Configuration) -> None:
+    config.sui_api_version.from_env("SWAGGER_UI_API_VERSION", as_=str, default="1.0.0")
+    config.sui_tab_title.from_env("SWAGGER_UI_TAB_TITLE", as_=str, required=True)
+    config.sui_page_title.from_env("SWAGGER_UI_PAGE_TITLE", as_=str, required=True)
+    config.sui_summary.from_env("SWAGGER_UI_SUMMARY", as_=str, required=True)
+    config.sui_description.from_env("SWAGGER_UI_DESCRIPTION", as_=str, required=True)
+    config.sui_redoc_logo_path.from_env("SWAGGER_UI_REDOC_LOGO_PATH", as_=str, required=True)
+    config.sui_redoc_logo_alt_text.from_env("SWAGGER_UI_REDOC_LOGO_ALT_TEXT", as_=str, required=True)
+    config.sui_redoc_logo_background_color.from_env("SWAGGER_UI_REDOC_LOGO_BACKGROUND_COLOR", as_=str, required=True)
+    config.sui_redoc_logo_href.from_env("SWAGGER_UI_REDOC_LOGO_HREF", as_=str, required=True)
+    config.sui_redoc_logo_width.from_env("SWAGGER_UI_REDOC_LOGO_WIDTH", as_=str, required=True)
+    config.sui_redoc_logo_height.from_env("SWAGGER_UI_REDOC_LOGO_HEIGHT", as_=str, required=True)
+
+
+class Container(DeclarativeContainer):
+    config = Configuration()
+    
+    build_db_config(config)
+    build_server_config(config)
+    build_swagger_ui_config(config)
---
+++ backend/src/server/factory.py
@@ -0,0 +1,47 @@
+from fastapi import FastAPI
+from fastapi.staticfiles import StaticFiles
+
+from src.core.fastapi.exceptions.handlers import init_error_handlers
+from src.core.fastapi.routing import add_routers
+from src.core.fastapi.openapi import apply_openapi_schema
+from src.modules.cbtracking.infrastructure.api import (
+    cbtracking_api_router,
+    endpoints as cbtracking_endpoints,
+)
+
+from .containers import Container
+
+
+def create_app() -> tuple[FastAPI, Container]:
+    """Build the FastAPi application instance."""
+
+    container = Container()
+    container.wire(modules=[cbtracking_endpoints])
+
+    wsconfig = container.config
+    debug: bool = wsconfig.server_debug()
+    root_path: str = wsconfig.server_api_root_path()
+
+    fastapi_app = FastAPI(
+        debug=debug,
+        title=wsconfig.sui_page_title(),
+        description=wsconfig.sui_description(),
+        root_path=f"/{root_path}",
+    )
+
+    fastapi_app.mount("/static", StaticFiles(directory="static"), name="static")
+    fastapi_app.container = container
+    fastapi_app.openapi = apply_openapi_schema(fastapi_app, wsconfig)
+
+    add_routers(
+        fastapi_app,
+        [cbtracking_api_router],
+        api_version=wsconfig.server_api_root_version(),
+    )
+    
+    init_error_handlers(fastapi_app, wsconfig.server_sysadmin_contact())
+
+    return fastapi_app, container
+
+
+app, container = create_app()
