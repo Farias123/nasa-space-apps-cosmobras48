1e37125359815d7d2d27adf2465fcc85f85318e6
Guilherme Lima Gonçalves <lwglguilherme@gmail.com>
2025-10-05 15:21:32 -0300

    fix: rectify stacks due to rebasing

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -5,7 +5,11 @@ import path from 'path';
 export default defineConfig({
   plugins: [react()],
   server: {
-    port: 3000
+    host: '0.0.0.0',
+    port: 3000,
+    watch: {
+      usePolling: true,
+    },
   },
   resolve: {
     alias: {
